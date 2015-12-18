23f05acb5fc638f78f7d7004dca86e79d722a053
robertsturner52 <robertsturner52@gmail.com>
2015-12-18 07:08:36 -0800

    handwritten exercise series

--- 32_Exercise Series/18.go
+++ 32_Exercise Series/18.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-
+	var indef interface{}
+	fmt.Println(indef)
+	fmt.Printf("%T\n", indef)
+	indef = "laughing cow"
+	fmt.Println(indef)
+	fmt.Printf("%T\n", indef)
+	str := indef.(string)
+	fmt.Println(str)
+	fmt.Printf("%T\n", str)
 }
-
--- 32_Exercise Series/19.go
+++ 32_Exercise Series/19.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-
+	slints := []int{1, 2, 3, 5, 8, 13, 21, 34}
+	for i, val := range slints {
+		fmt.Println(i, " - ", val)
+	}
 }
 
