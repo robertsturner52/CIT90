5ced0afd9a84541026c5bcd78873e618e1724802
robertsturner52 <robertsturner52@gmail.com>
2015-12-18 06:22:07 -0800

    handwritten exercise series

--- 32_Exercise Series/13.go
+++ 32_Exercise Series/13.go
@@ -1,8 +1,15 @@
 package main
 
 import "fmt"
+import "strconv"
 
 func main() {
-
+	num := 42
+	strNum := strconv.Itoa(num)
+	fmt.Println(strNum)
+	fmt.Printf("%T\n", strNum)
+	numStr, _ := strconv.Atoi(strNum)
+	fmt.Println(numStr)
+	fmt.Printf("%T\n", numStr)
 }
 
