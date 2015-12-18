f0d0b66ce8d30c621fceca3cf86134d7bf89e0e9
robertsturner52 <robertsturner52@gmail.com>
2015-12-18 07:40:47 -0800

    handwritten exercise series

--- 32_Exercise_Series/23.go
+++ 32_Exercise_Series/23.go
@@ -1,6 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	slints := []int{1, 2, 3, 5, 8, 13, 21, 34}
+	slintNew := []int{45, 79}
+	slints = append(slints, slintNew...)
+	fmt.Println(slints)
 }
 
