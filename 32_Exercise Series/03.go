e16e2a496b6b25a4ddcc179658f522acedcd6d3e
robertsturner52 <robertsturner52@gmail.com>
2015-12-18 04:37:04 -0800

    handwritten exercise series

--- 32_Exercise Series/03.go
+++ 32_Exercise Series/03.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	str := "Bob"
+	i := 42
+	truth := true
+	fmt.Printf("%T\n", str)
+	fmt.Printf("%T\n", i)
+	fmt.Printf("%T\n", truth)
 }
--- 32_Exercise Series/04.go
+++ 32_Exercise Series/04.go
@@ -1,6 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var i int
+	fmt.Println("The number is", i)
+	fmt.Println("Type a number:")
+	fmt.Scan(&i)
+	fmt.Println("The number is now", i)
 }
 
