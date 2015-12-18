c4801d57dc2d22f47ac172752f00d21ff54d7c6e
robertsturner52 <robertsturner52@gmail.com>
2015-12-18 05:06:33 -0800

    handwritten exercise series

--- 32_Exercise Series/06.go
+++ 32_Exercise Series/06.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-
+	var r rune
+	for i := 0; i < 256; i++ {
+		r = rune(i)
+		fmt.Println(i, "-", string(r))
+	}
 }
 
--- 32_Exercise Series/07.go
+++ 32_Exercise Series/07.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-
+	var str string
+	str = `raw string literal`
+	fmt.Println(str)
 }
 
